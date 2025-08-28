79d2d4f27a2ce58f7675046fc8a535a3f51411c4
Yuto299 <ota.se.ke@outlook.jp>
2025-08-28 18:41:10 +0900

    画面表示は完了（map直書き改善の余地あり）

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+import Notes from "./Notes";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      {Notes.map((note) => (
+        <Note key={note.key} title={note.title} text={note.content} />
+      ))}
+      <Footer />
+    </>
+  );
 }
 
 export default App;
