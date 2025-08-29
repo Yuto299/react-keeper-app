58647f2bb3aeff2f6cd27087471fc2a3be4f206e
Yuto299 <ota.se.ke@outlook.jp>
2025-08-29 21:26:54 +0900

    関数に分けて、可読性を向上させた

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,17 @@ import Header from "./Header";
 import Note from "./Note";
 import Notes from "./Notes";
 
+function createNotes(noteItem) {
+  return (
+    <Note key={noteItem.key} title={noteItem.title} text={noteItem.content} />
+  );
+}
+
 function App() {
   return (
     <>
       <Header />
-      {Notes.map((note) => (
-        <Note key={note.key} title={note.title} text={note.content} />
-      ))}
+      {Notes.map(createNotes)}
       <Footer />
     </>
   );
